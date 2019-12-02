be9a97e34d3faff5db934e4a1fb342caa6930edd
sauron <sauron998@gmail.com>
2019-12-02 14:38:25 +0800

    服务器配置 mysql redis
    Signed-off-by: sauron <sauron998@gmail.com>

---
+++ ctrls/apis/orderApi/order_api.go
@@ -0,0 +1,23 @@
+//============================================================
+// 描述:
+// 作者: Simon
+// 日期: 2019/12/2 2:36 下午
+//
+//============================================================
+
+package orderApi
+
+import "github.com/labstack/echo"
+
+func Create(ctx echo.Context) error {
+
+	return nil
+}
+
+func Update(ctx echo.Context) error {
+	return nil
+}
+
+func QueryPage(ctx echo.Context) error {
+	return nil
+}
--- ctrls/routes/routes.go
+++ ctrls/routes/routes.go
@@ -10,6 +10,8 @@ package routes
 import (
 	"net/http"
 
+	"github.com/yunlian/smart/ctrls/apis/orderApi"
+
 	"github.com/yunlian/smart/ctrls/apis/userApi"
 
 	"github.com/labstack/echo"
@@ -24,6 +26,13 @@ func InitRoutes(e *echo.Echo) {
 	initUserRoute(e)
 }
 
+func initSEOrderRoute(e *echo.Echo) {
+	g := e.Group("/api/order")
+	g.POST("/create", orderApi.Create)
+	g.POST("/update", orderApi.Update)
+	g.POST("/query", orderApi.QueryPage)
+}
+
 func initUserRoute(e *echo.Echo) {
 	//这里可以将一组api 添加拦截器,类似 servlet 中的 filter
 	g := e.Group("/api/user")
@@ -31,6 +40,7 @@ func initUserRoute(e *echo.Echo) {
 	g.POST("/register", userApi.Register)
 
 	g.GET("/page/:page/:size", userApi.List)
+
 }
 
 func ApiErrHandler(e error, c echo.Context) {
